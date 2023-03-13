f4c4559ed23e7b0300b631633ccbcb6598a02472
Levi Yan <43806292+leviyanx@users.noreply.github.com>
2023-03-13 23:52:05 +0800

    feat: add struct wasm module that implement the method of interface WasmModule

--- wasm_module.go
+++ wasm_module.go
@@ -1,7 +1,6 @@
 package containerd
 
 import (
-	"context"
 	"github.com/containerd/containerd/wasmmodules"
 )
 
@@ -14,8 +13,30 @@ type WasmModule interface {
 	Target() wasmmodules.WasmModuleSpec
 
 	// Size return the size of the wasm module
-	Size(ctx context.Context) (int64, error)
+	Size() (int64, error)
 
-	// Metadata returns the underlying wasm module metadate
+	// Metadata returns the underlying wasm module metadata
 	Metadata() wasmmodules.WasmModule
+}
+
+type wasmModule struct {
+	client *Client
+
+	wasmModule wasmmodules.WasmModule
+}
+
+func (w *wasmModule) Metadata() wasmmodules.WasmModule {
+	return w.wasmModule
+}
+
+func (w *wasmModule) Name() string {
+	return w.wasmModule.Name
+}
+
+func (w *wasmModule) Target() wasmmodules.WasmModuleSpec {
+	return w.wasmModule.Target
+}
+
+func (w *wasmModule) Size() (int64, error) {
+	return w.wasmModule.Target.Size, nil
 }
