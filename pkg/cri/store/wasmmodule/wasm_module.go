61eedcb4d3eaa24fc25efb96eb9664c54aa8f95a
Levi Yan <leviyanx@gmail.com>
2023-03-17 15:51:54 +0800

    refactor: clear the id function of wasm module

--- pkg/cri/store/wasmmodule/wasm_module.go
+++ pkg/cri/store/wasmmodule/wasm_module.go
@@ -4,12 +4,13 @@ import (
 	"github.com/containerd/containerd"
 	"github.com/containerd/containerd/wasmmodules"
 	"sync"
+	"time"
 )
 
 // WasmModule (cache) contains all resources associated with the wasm module. All fields Must not be mutated
 // directly after created.
 type WasmModule struct {
-	// ID of the wasm module.
+	// ID of the wasm module. Normally the digest of wasm module
 	ID string
 
 	// ChainID is the chainID of the wasm module
@@ -19,13 +20,13 @@ type WasmModule struct {
 
 	// WasmModuleSpec describes basic information about the wasm module
 	WasmModuleSpec wasmmodules.WasmModuleSpec
+
+	CreatedAt, UpdatedAt time.Time
 }
 
 // Store stores all wasm modules
 type Store struct {
 	lock sync.RWMutex
-
-	refCache map[string]string
 
 	// client is the containerd client
 	client *containerd.Client
@@ -35,8 +36,9 @@ type Store struct {
 }
 
 type store struct {
-	lock        sync.RWMutex
+	lock sync.RWMutex
+	// map: id - wasm module
 	wasmModules map[string]WasmModule
-	// ID set
-	idSet
+	// wasm module id set (id - image)
+	idSet map[string]string
 }
