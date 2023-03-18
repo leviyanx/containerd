2c9615b7f42e530849dd1d52c6566e2586ea4aff
Levi Yan <leviyanx@gmail.com>
2023-03-18 16:41:49 +0800

    feat: add script for cri integration test after adding wasm

---
+++ cri-integration-test-after-adding-wasm.sh
@@ -0,0 +1,15 @@
+# clean last build
+make clean
+
+# install containerd
+make GO_BUILDTAGS="no_btrfs"
+
+# start containerd
+sudo ./bin/containerd &
+
+# test
+cd integration
+sudo "PATH=$PATH" env go test -v -run "TestWasmModuleInCri" . -test.v
+
+# return to root
+cd ..
