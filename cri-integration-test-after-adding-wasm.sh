5373b2dfa326d972c516f6c6d94d7ea6b6e84ca4
Levi Yan <leviyanx@gmail.com>
2023-03-28 10:08:58 +0800

    refactor: separate the step that start containerd from the cri test script

--- cri-integration-test-after-adding-wasm.sh
+++ cri-integration-test-after-adding-wasm.sh
@@ -1,13 +1,3 @@
-# clean last build
-make clean
-
-# install containerd
-make GO_BUILDTAGS="no_btrfs"
-
-# start containerd
-sudo ./bin/containerd &
-containerd_pid=$!
-
 # test
 cd integration
 sudo "PATH=$PATH" env go test -v -run "TestWasmModuleInCri" . -test.v
---
+++ start-containerd.sh
@@ -0,0 +1,8 @@
+# clean last build
+make clean
+
+# install containerd
+make GO_BUILDTAGS="no_btrfs"
+
+# start containerd
+sudo ./bin/containerd &
