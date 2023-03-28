2d56344d22e88a8db041dbc836a0a028552b52b8
Levi Yan <leviyanx@gmail.com>
2023-03-28 12:36:26 +0800

    test: benchmark script add oci image benchmark

--- benchmark-between-oci-and-wasm.sh
+++ benchmark-between-oci-and-wasm.sh
@@ -2,6 +2,7 @@ cd integration/benchmark
 
 # benchmark test
 sudo "PATH=$PATH" env go test -bench="BenchmarkWasmModuleInCri" -count=3 . > bench.log
+sudo "PATH=$PATH" env go test -bench="BenchmarkImageInCri" -count=3 . >> bench.log
 
 # return to root
 cd ..
