a42ba1ba7bb40f50dc71027e37ba91a463c335c9
Levi Yan <leviyanx@gmail.com>
2023-03-28 11:58:18 +0800

    update benchmark script

--- benchmark-between-oci-and-wasm.sh
+++ benchmark-between-oci-and-wasm.sh
@@ -1,6 +1,7 @@
-cd integration
+cd integration/benchmark
+
 # benchmark test
-sudo "PATH=$PATH" env go test -bench="BenchmarkWasmModuleInCri" -benchtime=10x .
+sudo "PATH=$PATH" env go test -bench="BenchmarkWasmModuleInCri" -count=3 . > bench.log
 
 # return to root
 cd ..
