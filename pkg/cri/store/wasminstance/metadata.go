4db654f69dfc64bd3a13479730ff7861fbdfd3ee
Levi Yan <43806292+leviyanx@users.noreply.github.com>
2023-04-03 18:32:25 +0800

    feat: add some fields that includes runtime, created and update time

--- pkg/cri/store/wasminstance/metadata.go
+++ pkg/cri/store/wasminstance/metadata.go
@@ -1,18 +1,40 @@
 package wasminstance
 
-import runtime "k8s.io/cri-api/pkg/apis/runtime/v1"
+import (
+	"github.com/containerd/containerd/containers"
+	runtime "k8s.io/cri-api/pkg/apis/runtime/v1"
+	"time"
+)
 
 type Metadata struct {
 	// ID is the wasm instance id.
+	//
+	// This property is required and cannot be changed after creation.
 	ID string
+
 	// Name is the wasm instance name.
 	Name string
+
 	// WasmModuleID is the wasm module id the wasm instance belongs to.
 	SandboxID string
+
 	// Config is the CRI container config.
 	Config *runtime.ContainerConfig
+
 	// WasmModuleName is the name of the wasm module used by the wasm instance.
 	WasmModuleName string
+
 	// LogPath is the wasm instance log path.
 	StopSignal string
+
+	// Runtime specifies which runtime should be used when lanuching the wasm instance tasks.
+	//
+	// This property is required and immutable.
+	Runtime containers.RuntimeInfo
+
+	// CreatedAt is the time at which the container was created.
+	CreatedAt time.Time
+
+	// UpdatedAt is the time at which the container was updated.
+	UpdatedAt time.Time
 }
