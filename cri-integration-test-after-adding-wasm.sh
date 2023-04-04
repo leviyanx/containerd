cc196226502488f592cf700d87866818c1d432d7
Levi Yan <43806292+leviyanx@users.noreply.github.com>
2023-04-04 10:49:39 +0800

    test(cri test after adding wasm): add wasm instance restart test

--- cri-integration-test-after-adding-wasm.sh
+++ cri-integration-test-after-adding-wasm.sh
@@ -1,6 +1,7 @@
 # test
 cd integration
 sudo "PATH=$PATH" env go test -v -run "TestWasmModuleInCri" . -test.v
+sudo "PATH=$PATH" env go test -v -run "TestWasmInstanceRestart" -runtime-handler=wasm . -test.v
 
 # return to root
 cd ..
