e69b4f839815b38a4661296d825a9e746046f5ee
Levi Yan <43806292+leviyanx@users.noreply.github.com>
2023-04-10 18:35:32 +0800

    feat(StopWasmInstance): get wasm task and handle unknown state

--- pkg/cri/server/wasm_instance_stop.go
+++ pkg/cri/server/wasm_instance_stop.go
@@ -2,8 +2,12 @@ package server
 
 import (
 	"context"
+	"fmt"
+	eventtypes "github.com/containerd/containerd/api/events"
+	"github.com/containerd/containerd/errdefs"
 	"github.com/containerd/containerd/log"
 	"github.com/containerd/containerd/pkg/cri/store/wasminstance"
+	ctrdutil "github.com/containerd/containerd/pkg/cri/util"
 	runtime "k8s.io/cri-api/pkg/apis/runtime/v1"
 	"time"
 )
@@ -33,9 +37,41 @@ func (c *criService) stopWasmInstance(ctx context.Context, wasmInstance wasminst
 		return nil
 	}
 
-	// TODO: Get wasm task
+	// Get wasm task
+	wasmTask, err := wasmInstance.Task(ctx, nil)
+	if err != nil {
+		if !errdefs.IsNotFound(err) {
+			return fmt.Errorf("failed to get task for wasm instance %q: %w", id, err)
+		}
+		// Don't return for unknown state, some cleanup needs to be done.
+		if state == runtime.ContainerState_CONTAINER_UNKNOWN {
+			return cleanupUnknownWasmInstance(ctx, id, wasmInstance)
+		}
+		return nil
+	}
 
-	// TODO: Handle unknown state
+	// Handle unknown state
+	if state == runtime.ContainerState_CONTAINER_UNKNOWN {
+		// Start an exit handler for wasm instances in unknown state.
+		waitCtx, waitCancel := context.WithCancel(ctrdutil.NamespacedContext())
+		defer waitCancel()
+		exitCh, err := wasmTask.Wait(waitCtx)
+		if err != nil {
+			if !errdefs.IsNotFound(err) {
+				return fmt.Errorf("failed to wait for wasm instance %q: %w", id, err)
+			}
+			return cleanupUnknownWasmInstance(ctx, id, wasmInstance)
+		}
+
+		exitCtx, exitCancel := context.WithCancel(context.Background())
+		stopCh := c.eventMonitor.startWasmInstanceExitMonitor(exitCtx, id, wasmTask.Pid(), exitCh)
+		defer func() {
+			exitCancel()
+			// This ensures that the exit monitor is stopped before `Wait` is canceled,
+			// so no exit event is generated because of the `Wait` cancellation.
+			<-stopCh
+		}()
+	}
 
 	// TODO: Kill the wasm task belongs to wasm instance
 
@@ -44,6 +80,18 @@ func (c *criService) stopWasmInstance(ctx context.Context, wasmInstance wasminst
 	// TODO: Wait for a fixed time until wasm instance stop is observed by event monitor
 
 	return nil
+}
+
+// cleanupUnknownWasmInstance cleans up stopped wasm instance in unknown state.
+func cleanupUnknownWasmInstance(ctx context.Context, id string, wasmInstance wasminstance.WasmInstance) error {
+	// Reuse handleWasmInstanceExit to do the cleanup
+	return handleWasmInstanceExit(ctx, &eventtypes.WasmTaskExit{
+		WasmInstanceID: id,
+		ID:             id,
+		Pid:            0,
+		ExitStatus:     unknownExitCode,
+		ExitedAt:       time.Now(),
+	}, wasmInstance)
 }
 
 func criWasmInstanceStateToString(state runtime.ContainerState) string {
