31062fff7eab379216fc857e0b9a3a0ea5599bad
Levi Yan <43806292+leviyanx@users.noreply.github.com>
2023-04-16 21:23:25 +0800

    feat(RemoveWasmInstance): set removing state to prevent other start/remove operations against this wasm instance while it's being removed.

--- pkg/cri/server/wasm_instance_remove.go
+++ pkg/cri/server/wasm_instance_remove.go
@@ -2,6 +2,7 @@ package server
 
 import (
 	"context"
+	"errors"
 	"fmt"
 	"github.com/containerd/containerd/pkg/cri/store/wasminstance"
 	"github.com/sirupsen/logrus"
@@ -23,7 +24,36 @@ func (c *criService) RemoveWasmInstance(ctx context.Context, wasmInstance *wasmi
 		}
 	}
 
+	// Set removing state to prevent other start/remove operations against this wasm instance
+	// while it's being removed.
+	if err := setWasmInstanceRemoving(wasmInstance); err != nil {
+		return nil, fmt.Errorf("failed to set removing state for wasm instance %q: %w", id, err)
+	}
+
 	wasmInstanceRemoveTimer.WithValues(wasmInstance.Runtime.Name).UpdateSince(start)
 
 	return &runtime.RemoveContainerResponse{}, nil
+}
+
+// setWasmInstanceRemoving sets the removing state for the given wasm instance. In removing state, the
+// wasm instance can't be started or removed.
+func setWasmInstanceRemoving(wasmInstance *wasminstance.WasmInstance) error {
+	return wasmInstance.Status.Update(func(status wasminstance.Status) (wasminstance.Status, error) {
+		// Do not remove wasm instance if it's still running or unknown.
+		if status.State() == runtime.ContainerState_CONTAINER_RUNNING {
+			return status, errors.New("wasm instance is still running, to stop first")
+		}
+		if status.State() == runtime.ContainerState_CONTAINER_UNKNOWN {
+			return status, errors.New("wasm instance is in unknown state, to stop first")
+		}
+		if status.Starting {
+			return status, errors.New("wasm instance is in starting state, can't be removed")
+		}
+		if status.Removing {
+			return status, errors.New("wasm instance is already in removing state")
+		}
+
+		status.Removing = true
+		return status, nil
+	})
 }
