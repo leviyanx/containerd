f5815768b79fb9057fc6d2b994ce2c3bbc9b7c95
Levi Yan <43806292+leviyanx@users.noreply.github.com>
2023-04-18 00:20:48 +0800

    feat: add script that execute benchmark about container operations

--- benchmark-between-oci-and-wasm.sh
+++ benchmark-between-oci-and-wasm.sh
@@ -1,8 +1,11 @@
 cd integration/benchmark
 
 # benchmark test
-sudo "PATH=$PATH" env go test -bench="BenchmarkWasmModuleInCri" -count=3 . > bench.log
-sudo "PATH=$PATH" env go test -bench="BenchmarkImageInCri" -count=3 . >> bench.log
+#sudo "PATH=$PATH" env go test -bench="BenchmarkWasmModuleInCri" -count=3 . > bench.log
+#sudo "PATH=$PATH" env go test -bench="BenchmarkImageInCri" -count=3 . >> bench.log
+
+sudo "PATH=$PATH" env go test -bench="BenchmarkWasmInstanceInCri" -runtime-handler=wasm . > ../../bench.log
+sudo "PATH=$PATH" env go test -bench="BenchmarkContainerInCri" . >> ../../bench.log
 
 # return to root
 cd ..
