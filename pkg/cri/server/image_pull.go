4e24217db20781930eca7e4d251a6d61cbab9a76
youtirsin <youtirsin@qq.com>
2023-04-20 14:28:25 +0800

    fix: ignore pulling if wasm module exists

--- pkg/cri/server/image_pull.go
+++ pkg/cri/server/image_pull.go
@@ -97,6 +97,13 @@ func (c *criService) PullImage(ctx context.Context, r *runtime.PullImageRequest)
 	// Check whether the imageSpec contains a "wasm." annotation.
 	if wasmmodule.IsWasmModule(r.GetImage()) {
 		wasmModuleName := r.GetImage().GetImage()
+    // check whether module already exists
+    resolvedID, err := c.wasmModuleStore.Resolve(wasmModuleName)
+    if err == nil {
+			logrus.Warningf("image already exists, for now we ignore pulling.")
+      return &runtime.PullImageResponse{ImageRef: resolvedID}, nil
+    }
+
 		wasmModuleUrl := r.GetImage().GetAnnotations()["wasm.module.url"]
 		// filename is required, used to name the wasm module file
 		wasmModuleFilename, filenameExist := r.GetImage().GetAnnotations()["wasm.module.filename"]
